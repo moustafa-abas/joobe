d2eb0282c5d931ddc5ffc1af5c25d5600a1c1e22
moustafa-abas <124290544+moustafa-abas@users.noreply.github.com>
2024-07-28 20:34:39 +0300

    comment test page

--- src/pages/registration/test.jsx
+++ src/pages/registration/test.jsx
@@ -1,21 +1,21 @@
 
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode";
-const Test = () => {
+// import { GoogleLogin } from '@react-oauth/google';
+// import { jwtDecode } from "jwt-decode";
+// const Test = () => {
 
 
-  return (
+//   return (
 
-    <GoogleLogin
-      onSuccess={credentialResponse => {
-        const credentialResponseDecoded=jwtDecode(credentialResponse.credential)
-        console.log(credentialResponseDecoded);
-      }}
-      onError={() => {
-        console.log('Login Failed');
-      }}
-    />
-  )
-}
+//     <GoogleLogin
+//       onSuccess={credentialResponse => {
+//         const credentialResponseDecoded=jwtDecode(credentialResponse.credential)
+//         console.log(credentialResponseDecoded);
+//       }}
+//       onError={() => {
+//         console.log('Login Failed');
+//       }}
+//     />
+//   )
+// }
 
-export default Test
+// export default Test
